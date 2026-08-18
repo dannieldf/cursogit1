bcbec94b0891925bc62d1244d1109dafd8b27f78
Daniel Castro Machado <danielmachado@mpf.mp.br>
2026-08-18 13:46:16 -0300

    Métodos em PesquisaManutencao

--- PesquisaManutencao.php
+++ PesquisaManutencao.php
@@ -1,5 +1,25 @@
 <?php
-/*
-main
-*/
+class PesquisaManutencao
+{
+    public static function metodo1()
+    {
+        /*
+        codigo 1
+        */
+    }
+
+    public static function metodo2()
+    {
+        /*
+        codigo 2
+        */
+    }
+
+    public static function metodo3()
+    {
+        /*
+        codigo 3
+        */
+    }
+}
 ?>
