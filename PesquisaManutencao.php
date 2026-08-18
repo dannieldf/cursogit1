5c5444ec4298d436ee2f605b013b36aecf7029a0
Daniel Castro Machado <danniel.df@hotmail.com.br>
2026-08-18 13:49:39 -0300

    Update 2 PesquisaManutencao.php

--- PesquisaManutencao.php
+++ PesquisaManutencao.php
@@ -3,17 +3,17 @@ class PesquisaManutencao
 {
     public static function metodo1()
     {
-        echo "codigo 1";
+        echo "codigo 1 plus";
     }
 
     public static function metodo2()
     {
-        echo "codigo 2";
+        echo "codigo 2 plus";
     }
 
     public static function metodo3()
     {
-        echo "codigo 3";
+        echo "codigo 3 plus";
     }
 }
 ?>
